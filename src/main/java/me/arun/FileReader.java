2ab4e7042771b473ace52dcb543ff37edc0a4ed8
Arunkumar Paramasivam <aparamasivam@Arunkumars-MacBook-Pro.local>
2019-03-07 09:15:51 +1100

    read single file content as string

--- src/main/java/me/arun/FileReader.java
+++ src/main/java/me/arun/FileReader.java
@@ -1,9 +1,12 @@
 package me.arun;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class FileReader
 {
-   public String read(String fileName){
-
-       return fileName;
+   public String read(String fileName) throws IOException {
+       return new String(Files.readAllBytes(Paths.get(fileName)));
    }
 }
--- src/test/java/me/arun/FileReaderTest.java
+++ src/test/java/me/arun/FileReaderTest.java
@@ -7,14 +7,15 @@ import static org.junit.Assert.assertTrue;
 import org.hamcrest.core.Is;
 import org.junit.Test;
 
+import java.io.IOException;
+
 public class FileReaderTest
 {
     @Test
-    public void shouldReadFileContent()
-    {
+    public void shouldReadFileContent() throws IOException {
 
         FileReader fileReader =  new FileReader();
-        String content = fileReader.read("test.sql");
+        String content = fileReader.read("./src/test/resources/test.sql");
 
         assertThat(content, is( "select * from table;"));
     }
