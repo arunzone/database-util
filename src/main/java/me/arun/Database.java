8529e577ad50ef505e96317542a52d055d30c79b
Arun <aruncontacts@gmail.com>
2019-03-14 18:04:57 +1100

    execute query integration test

--- src/main/java/me/arun/Database.java
+++ src/main/java/me/arun/Database.java
@@ -2,19 +2,49 @@ package me.arun;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Database {
     private DriverType driverType;
+    private Connection connection;
 
-    public Database(DriverType driverType) {
+    public Database(DriverType driverType) throws SQLException, ClassNotFoundException {
         this.driverType = driverType;
+        this.connection = establishConnection();
     }
 
-    public Connection connection() throws SQLException, ClassNotFoundException {
+    public Connection connection(){
+        return connection;
+    }
+    private Connection establishConnection() throws SQLException, ClassNotFoundException {
         Class.forName(driverType.getDriverName());
         return DriverManager.getConnection(driverType.getDriverUrl(), "aconexsql", "aconexsql");
     }
 
 
+    public List<Map<String, Object>> executeQuery(String query) throws SQLException {
+        Statement stmt=connection().createStatement();
+        return resultSetToList(stmt.executeQuery(query));
+    }
+
+    private List<Map<String, Object>> resultSetToList(ResultSet resultSet) throws SQLException {
+        final ResultSetMetaData metaData = resultSet.getMetaData();
+        int columnsCount = metaData.getColumnCount();
+        List<Map<String, Object>> rows = new ArrayList<Map<String, Object>>();
+        while (resultSet.next()){
+            Map<String, Object> row = new HashMap<String, Object>(columnsCount);
+            for(int i = 1; i <= columnsCount; ++i){
+                row.put(metaData.getColumnName(i), resultSet.getObject(i));
+            }
+            rows.add(row);
+        }
+        return rows;
+    }
 }
--- src/test/java/me/arun/DatabaseTest.java
+++ src/test/java/me/arun/DatabaseTest.java
@@ -1,11 +1,19 @@
 package me.arun;
 
+import org.hamcrest.collection.IsEmptyCollection;
 import org.hamcrest.core.Is;
+import org.hamcrest.core.IsCollectionContaining;
+import org.hamcrest.core.IsNot;
 import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
 import static org.junit.Assert.assertThat;
 
 public class DatabaseTest {
@@ -14,6 +22,15 @@ public class DatabaseTest {
         Database database = new Database(DriverType.JTDS);
 
         assertThat(database.connection(), Is.isA(Connection.class));
+    }
+
+    @Test
+    public void shouldExecuteQuery() throws SQLException, ClassNotFoundException {
+        Database database =  new Database(DriverType.JTDS);
+
+        List<Map<String, Object>> result = database.executeQuery("select * from userx");
+
+        assertThat(result, is(not(empty())));
     }
 
 }
